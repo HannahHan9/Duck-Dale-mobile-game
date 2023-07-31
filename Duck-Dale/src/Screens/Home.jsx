3614910dcb4af8404ff88fa01317b06f189d41c4
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-31 16:25:43 +0100

    make home landscape

--- Duck-Dale/src/Screens/Home.jsx
+++ Duck-Dale/src/Screens/Home.jsx
@@ -5,42 +5,26 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import Orientation from "react-native-orientation";
 
 export default function Home() {
-    useEffect(() => {
-        lockOrientation();
-      }, []);
-    return <View>
-        <Text>Hi Lucy!!</Text>
-    </View>;
+	useEffect(() => {
+		lockOrientation();
+	}, []);
+	const lockOrientation = async () => {
+		await ScreenOrientation.lockAsync(
+			ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT
+		);
+	};
+	return (
+		<View style={styles.container}>
+			<Text>Hi Lucy!!</Text>
+		</View>
+	);
 }
 
-
-
-
-
-
-//   const [orientation, setOrientation] = useState(1);
-//   useEffect(() => {
-//     lockOrientation();
-//   }, []);
-//   const lockOrientation = async () => {
-//     await ScreenOrientation.lockAsync(
-//       ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT
-//     );
-//   const o = await ScreenOrientation.getOrientationAsync();
-//     setOrientation(o);
-//   };
-//   return (
-//     <View style={styles.container}>
-//       <Text>Locked Screen orientation: {orientation}</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
