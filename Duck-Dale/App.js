f1be092a65146e9acbf11833ee1d869ec8356d06
LucyRTCodes <lucyrtutton@gmail.com>
2023-07-31 12:29:51 +0100

    complete registration view

--- Duck-Dale/App.js
+++ Duck-Dale/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {LogInRegister} from './src/Screens/LogInRegister'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LogInRegister from "./src/Screens/LogInRegister";
 
 export default function App() {
-  return (
-    <View>
-      <LogInRegister style={styles.container}/>
-    </View>
-  );
+	return <LogInRegister style={styles.container} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
