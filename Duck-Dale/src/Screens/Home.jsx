545aa563ed6eb30827672c80d259efc72039b34f
HannahHan9 <h.han9204@gmail.com>
2023-08-01 10:41:16 +0100

    fix the app to landscape mode

--- Duck-Dale/src/Screens/Home.jsx
+++ Duck-Dale/src/Screens/Home.jsx
@@ -27,6 +27,7 @@ export default function Home() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		flexDirection: 'row',
 		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
