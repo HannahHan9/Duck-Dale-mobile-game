643810ec8da256204894fd26ccdf2b930b7a49c1
LucyRTCodes <lucyrtutton@gmail.com>
2023-08-02 18:50:18 +0100

    add some functionality to buy and sell

--- Duck-Dale/src/Components/Buy.jsx
+++ Duck-Dale/src/Components/Buy.jsx
@@ -6,47 +6,50 @@ import { CoinContext } from "../Contexts/CoinContext";
 import Item from "./Item";
 
 function Buy() {
-    const { user } = useContext(UserContext);
-    const { setCoins } = useContext(CoinContext);
-    const [items, setItems] = useState([]);
+	const { user } = useContext(UserContext);
+	const { setCoins } = useContext(CoinContext);
+	const [items, setItems] = useState([]);
+	const [buyChoices, setBuyChoices] = useState([]);
 
-    useEffect(() => {
-        getAllShopItems().then((items) => {
-            setItems(items);
-        });
-    }, []);
-    return (
-        <View>
-            <View style={styles.container}>
-                <Text style={[styles.titles, { textAlign: "left" }]}>Item</Text>
-                <Text style={styles.titles}>Price</Text>
-                <Text style={styles.titles}>Quantity</Text>
-            </View>
+	useEffect(() => {
+		getAllShopItems().then((items) => {
+			setItems(items);
+		});
+	}, []);
+	return (
+		<View>
+			<View style={styles.container}>
+				<Text style={[styles.titles, { textAlign: "left", flex: 0.2 }]}>
+					Quantity
+				</Text>
+				<Text style={styles.titles}>Item</Text>
+				<Text style={styles.titles}>Price</Text>
+			</View>
 
-            <ScrollView>
-                {items.map((item) => {
-                    if (item.username === user && item.quantity > 0) {
-                        return (
+			<ScrollView>
+				{items.map((item) => {
+					if (item.username === user && item.quantity > 0) {
+						return (
 							<View key={item._id}>
-                            <Item
-                                item_name={item.item_name}
-                                price={item.price}
-                                quantity={item.quantity}
-                                id={item._id}
-                            />
+								<Item
+									item_name={item.item_name}
+									price={item.price}
+									quantity={item.quantity}
+									setChoices={setBuyChoices}
+								/>
 							</View>
-                        );
-                    }
-                })}
-            </ScrollView>
-        </View>
-    );
+						);
+					}
+				})}
+			</ScrollView>
+			{buyChoices.length ? <Button title="Buy"></Button> : null}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: { flexDirection: "row" },
-    titles: { flex: 0.2, textAlign: "center" },
-    items: { flex: 0.25, textAlign: "center" },
+	container: { flexDirection: "row" },
+	titles: { flex: 0.4, textAlign: "center", fontSize: 20, fontWeight: "bold" },
 });
 
 export default Buy;
--- Duck-Dale/src/Components/Item.jsx
+++ Duck-Dale/src/Components/Item.jsx
@@ -2,35 +2,53 @@ import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 import { getAllShopItems } from "../Lib/Api";
 import { useContext, useEffect, useState } from "react";
 
-const Item = ({ item_name, quantity, price, id }) => {
-    const [choices, setChoices] = useState([]);
+const Item = ({ item_name, quantity, price, setChoices }) => {
+	const [border, setBorder] = useState("transparent");
+	const handleSelect = () => {
+		setBorder((current) => {
+			return current === "transparent" ? "white" : "transparent";
+		});
+		setChoices((current) => {
+			if (current.includes(item_name)) {
+				return current.filter((item) => item !== item_name);
+			} else {
+				return [...current, item_name];
+			}
+		});
+	};
 
-    const handleSelect = (itemName) => {
-        if (choices.includes(itemName)) {
-        }
-    };
-
-    return (
-        <View>
-            <View style={styles.container}>
-                <Text style={[styles.items, { textAlign: "left" }]}>
-                    {item_name}
-                </Text>
-                <Text style={styles.items}>{price}</Text>
-                <Text style={styles.items}>{quantity}</Text>
-                <Button
-                    style={[styles.items, { width: 30 }]}
-                    title="Select"
-                ></Button>
-            </View>
-        </View>
-    );
+	return (
+		<View>
+			<View style={[styles.container, { backgroundColor: border }]}>
+				<Text
+					style={[styles.items, { textAlign: "left", flex: 0.2 }]}
+					onPress={handleSelect}
+				>
+					{quantity}
+				</Text>
+				<Text style={styles.items} onPress={handleSelect}>
+					{item_name}
+				</Text>
+				<Text style={styles.items} onPress={handleSelect}>
+					{price}🪙
+				</Text>
+				{/* <Button
+					style={[styles.items, { width: 30 }]}
+					title="Select"
+					onPress={handleSelect}
+				></Button> */}
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: { flexDirection: "row" },
-    titles: { flex: 0.2, textAlign: "center" },
-    items: { flex: 0.25, textAlign: "center" },
+	container: { flexDirection: "row" },
+	items: {
+		flex: 0.4,
+		textAlign: "center",
+		fontSize: 20,
+	},
 });
 
 export default Item;
