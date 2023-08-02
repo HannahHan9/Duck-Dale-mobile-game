9633a987c9c867ed0ef9fc0c9902609ae7eeedc4
LucyRTCodes <lucyrtutton@gmail.com>
2023-08-02 12:21:49 +0100

    format sell and buy components

--- Duck-Dale/src/Components/Buy.jsx
+++ Duck-Dale/src/Components/Buy.jsx
@@ -1,4 +1,4 @@
-import { ScrollView, Text, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 import { getAllShopItems } from "../Lib/Api";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../Contexts/UserContext";
@@ -13,15 +13,32 @@ function Buy() {
 	}, []);
 	return (
 		<View>
-			<Text>Buy</Text>
+			<View style={styles.container}>
+				<Text style={[styles.titles, { textAlign: "left" }]}>Item</Text>
+				<Text style={styles.titles}>Price</Text>
+				<Text style={styles.titles}>Quantity</Text>
+			</View>
+
 			<ScrollView>
 				{items.map((item) => {
-					if (item.username === user) {
+					if (item.username === user && item.quantity > 0) {
 						return (
-							<View style={{ flexDirection: "row", gap: 30 }}>
-								<Text style={{ flex: 0.5 }}>{item.item_name}</Text>
-								<Text>{item.price}</Text>
-								<Text>{item.quantity}</Text>
+							<View key={item._id}>
+								<View style={styles.container}>
+									<Text style={[styles.items, { textAlign: "left" }]}>
+										{item.item_name}
+									</Text>
+									<Text style={styles.items}>{item.price}</Text>
+									<Text style={styles.items}>{item.quantity}</Text>
+									<Button
+										style={[styles.items, { width: 30 }]}
+										title="Inc"
+									></Button>
+									<Button
+										style={[styles.items, { width: 30 }]}
+										title="Sell"
+									></Button>
+								</View>
 							</View>
 						);
 					}
@@ -30,5 +47,11 @@ function Buy() {
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: { flexDirection: "row" },
+	titles: { flex: 0.2, textAlign: "center" },
+	items: { flex: 0.25, textAlign: "center" },
+});
 
 export default Buy;
--- Duck-Dale/src/Components/Sell.jsx
+++ Duck-Dale/src/Components/Sell.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
 import { getAllUserItems } from "../Lib/Api";
 import { UserContext } from "../Contexts/UserContext";
 
@@ -13,15 +13,45 @@ function Sell() {
 	}, []);
 	return (
 		<View>
+			<View style={styles.container}>
+				<Text style={[styles.titles, { textAlign: "left" }]}>Item</Text>
+				<Text style={styles.titles}>Price</Text>
+				<Text style={styles.titles}>Quantity</Text>
+			</View>
+
 			<ScrollView>
 				{items.map((item) => {
-					if (item.username === user) {
-						return <Text>{item.item_name}</Text>;
+					if (item.username === user && item.quantity > 0) {
+						return (
+							<View key={item._id}>
+								<View style={styles.container}>
+									<Text style={[styles.items, { textAlign: "left" }]}>
+										{item.item_name}
+									</Text>
+									<Text style={styles.items}>{item.price}</Text>
+									<Text style={styles.items}>{item.quantity}</Text>
+									<Button
+										style={[styles.items, { width: 30 }]}
+										title="Inc"
+									></Button>
+									<Button
+										style={[styles.items, { width: 30 }]}
+										title="Sell"
+									></Button>
+								</View>
+							</View>
+						);
 					}
 				})}
 			</ScrollView>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: { flexDirection: "row" },
+	titles: { flex: 0.2, textAlign: "center" },
+	items: { flex: 0.25, textAlign: "center" },
+});
 
 export default Sell;
