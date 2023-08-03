fdbb59e87899c88149cb338e994246b1c613ff6e
LucyRTCodes <lucyrtutton@gmail.com>
2023-08-03 13:44:27 +0100

    styling headers in stack nav

--- Duck-Dale/src/Navigation/StackNavigator.jsx
+++ Duck-Dale/src/Navigation/StackNavigator.jsx
@@ -12,48 +12,49 @@ import { NewUserContext } from "../Contexts/NewUserContext";
 const Stack = createStackNavigator();
 
 export const AuthNavigator = () => {
-    const { user } = useContext(UserContext);
-    const { newUser } = useContext(NewUserContext);
+	const { user } = useContext(UserContext);
+	const { newUser } = useContext(NewUserContext);
 
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
-            {!user && !newUser ? (
-                <Stack.Screen name="LogInRegister" component={LogInRegister} />
-            ) : null}
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			{!user && !newUser ? (
+				<Stack.Screen name="LogInRegister" component={LogInRegister} />
+			) : null}
 
-            {user ? (
-                <Stack.Screen name="HomeNavigator" component={HomeNavigator} />
-            ) : null}
+			{user ? (
+				<Stack.Screen name="HomeNavigator" component={HomeNavigator} />
+			) : null}
 
-            {newUser ? (
-                <Stack.Screen
-                    name="CharacterCreation"
-                    component={CharacterCreation}
-                />
-            ) : null}
-        </Stack.Navigator>
-    );
+			{newUser ? (
+				<Stack.Screen name="CharacterCreation" component={CharacterCreation} />
+			) : null}
+		</Stack.Navigator>
+	);
 };
 
 export const HomeNavigator = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Farm" component={Farm} />
-            <Stack.Screen name="Shop" component={Shop} />
-        </Stack.Navigator>
-    );
+	return (
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Stack.Screen name="Home" component={Home} />
+			<Stack.Screen name="Farm" component={Farm} />
+			<Stack.Screen name="Shop" component={Shop} />
+		</Stack.Navigator>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#abc",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+	container: {
+		flex: 1,
+		backgroundColor: "#abc",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
