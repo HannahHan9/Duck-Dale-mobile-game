3f575c15e8841f893201de147b6729be4c95e289
LucyRTCodes <lucyrtutton@gmail.com>
2023-08-04 09:51:47 +0100

    begin pseudocode for farm

--- Duck-Dale/src/Screens/Farm.jsx
+++ Duck-Dale/src/Screens/Farm.jsx
@@ -5,3 +5,14 @@ function Farm() {
 }
 
 export default Farm;
+
+// Make Crop component
+// inside crop:
+// back-end: gridSquare and userItems
+// front-end: getUserItems
+// once selected => post selected item to gridSquare
+// start setTimeout (possibly with grow time value from back-end)
+// every 1/3rd of the grow time => change state of image src (from back-end data possibly)
+
+// scrollview
+// grid of touchable squares => handle planting/harvesting
