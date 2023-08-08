622066e7e590c95c86cd59ce885212d1c8984104
HannahHan9 <h.han9204@gmail.com>
2023-08-08 18:26:07 +0100

    add not enough coins error message

--- Duck-Dale/src/Components/Buy.jsx
+++ Duck-Dale/src/Components/Buy.jsx
@@ -1,17 +1,17 @@
 import {
-	Button,
-	ImageBackground,
-	ScrollView,
-	StyleSheet,
-	Text,
-	View,
+    Button,
+    ImageBackground,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
 } from "react-native";
 import {
-	getAllShopItems,
-	patchShopItems,
-	patchUserCoins,
-	patchUserItems,
-	postUserItems,
+    getAllShopItems,
+    patchShopItems,
+    patchUserCoins,
+    patchUserItems,
+    postUserItems,
 } from "../Lib/Api";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../Contexts/UserContext";
@@ -19,101 +19,116 @@ import { CoinContext } from "../Contexts/CoinContext";
 import Item from "./Item";
 
 function Buy() {
-	const { user } = useContext(UserContext);
-	const { coins, setCoins } = useContext(CoinContext);
-	const [items, setItems] = useState([]);
-	const [buyChoices, setBuyChoices] = useState([]);
-	const [error, setError] = useState(null);
-	const [isLoading, setIsLoading] = useState(false);
+    const { user } = useContext(UserContext);
+    const { coins, setCoins } = useContext(CoinContext);
+    const [items, setItems] = useState([]);
+    const [buyChoices, setBuyChoices] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [cost, setCost] = useState(0);
 
-	const handleBuy = () => {
-		setError(null);
-		setIsLoading(true);
-		setItems((current) => {
-			return current.filter((item) => !buyChoices.includes(item));
-		});
+    const handleBuy = () => {
+        setError(null);
+        setIsLoading(true);
+        setItems((current) => {
+            return current.filter((item) => !buyChoices.includes(item));
+        });
 
-		const addPromises = [];
-		const removePromises = [];
-		let total = 0;
-		buyChoices.forEach(({ item_name, quantity, price }) => {
-			console.log(quantity);
-			addPromises.push(patchUserItems(user, item_name, quantity));
-			removePromises.push(patchShopItems(user, item_name, -quantity));
-			total += price * quantity;
-		});
-		if (coins - total >= 0) {
-			Promise.all(addPromises)
-				.then(() => {
-					Promise.all(removePromises);
-				})
-				.then(() => {
-					return patchUserCoins(user, coins - total);
-				})
-				.then((money) => {
-					setCoins(money);
-				})
+        const addPromises = [];
+        const removePromises = [];
+        let total = 0;
+        buyChoices.forEach(({ item_name, quantity, price }) => {
+            console.log(quantity);
+            addPromises.push(patchUserItems(user, item_name, quantity));
+            removePromises.push(patchShopItems(user, item_name, -quantity));
+            total += price * quantity;
+        });
+        if (coins - total >= 0) {
+            Promise.all(addPromises)
+                .then(() => {
+                    Promise.all(removePromises);
+                })
+                .then(() => {
+                    return patchUserCoins(user, coins - total);
+                })
+                .then((money) => {
+                    setCoins(money);
+                })
 
-				.catch(() => {
-					console.log("it broked");
-				})
-				.finally(() => {
-					setBuyChoices([]);
-					setIsLoading(false);
-				});
-		} else {
-			setError("Not Enough Coins");
-		}
+                .catch(() => {
+                    console.log("it broked");
+                })
+                .finally(() => {
+                    setBuyChoices([]);
+                    setIsLoading(false);
+                });
+        } else {
+            setError("Not Enough Coins");
+        }
 
-		//check user inventory
-		//if exists => patch
-		//patch user coins
-	};
-	useEffect(() => {
-		getAllShopItems(user).then((items) => {
-			setItems(items);
-		});
-	}, [coins]);
-	return (
-		<ImageBackground
-			source={require("../../assets/backgrounds/wood-background.png")}
-			resizeMode="cover"
-			style={{ flex: 1, justifyContent: "center" }}
-		>
-			<View style={[styles.container, { backgroundColor: "white" }]}>
-				<Text style={[styles.titles, { textAlign: "left", flex: 1.2 }]}>
-					Quantity
-				</Text>
-				<Text style={styles.titles}>Item</Text>
-				<Text style={styles.titles}>Price</Text>
-			</View>
+        //check user inventory
+        //if exists => patch
+        //patch user coins
+    };
+    useEffect(() => {
+        getAllShopItems(user).then((items) => {
+            setItems(items);
+        });
+    }, [coins]);
+    return (
+        <ImageBackground
+            source={require("../../assets/backgrounds/wood-background.png")}
+            resizeMode="cover"
+            style={{ flex: 1, justifyContent: "center" }}
+        >
+            <View style={[styles.container, { backgroundColor: "white" }]}>
+                <Text style={[styles.titles, { textAlign: "left", flex: 1.2 }]}>
+                    Quantity
+                </Text>
+                <Text style={styles.titles}>Item</Text>
+                <Text style={styles.titles}>Price</Text>
+            </View>
 
-			<ScrollView>
-				{items.map((item) => {
-					if (item.quantity > 0) {
-						return (
-							<View key={item._id}>
-								{isLoading ? (
-									<Text>Loading</Text>
-								) : (
-									<Item item={item} setChoices={setBuyChoices} />
-								)}
-							</View>
-						);
-					}
-				})}
-			</ScrollView>
-			{buyChoices.length ? (
-				<Button title="Buy" onPress={handleBuy}></Button>
-			) : null}
-			{error ? <Text>{error}</Text> : null}
-		</ImageBackground>
-	);
+            <ScrollView>
+                {items.map((item) => {
+                    if (item.quantity > 0) {
+                        return (
+                            <View key={item._id}>
+                                {isLoading ? (
+                                    <Text>Loading</Text>
+                                ) : (
+                                    <Item
+                                        item={item}
+                                        setChoices={setBuyChoices}
+                                        setCost={setCost}
+                                    />
+                                )}
+                            </View>
+                        );
+                    }
+                })}
+            </ScrollView>
+            {buyChoices.length ? (
+                <Button
+                    title="Buy"
+                    onPress={handleBuy}
+					accessibilityLabel="Not enough coins!"
+                    disabled={cost > coins}
+                ></Button>
+            ) : null}
+            {error ? <Text>{error}</Text> : null}
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-	container: { flexDirection: "row" },
-	titles: { flex: 0.4, textAlign: "center", fontSize: 20, fontWeight: "bold" },
+    container: { flexDirection: "row" },
+    titles: {
+        flex: 0.4,
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
+    },
 });
 
 export default Buy;
--- Duck-Dale/src/Components/Item.jsx
+++ Duck-Dale/src/Components/Item.jsx
@@ -1,68 +1,77 @@
 import {
-	Button,
-	Image,
-	ScrollView,
-	StyleSheet,
-	Text,
-	View,
+    Button,
+    Image,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
 } from "react-native";
 import { getAllShopItems } from "../Lib/Api";
 import { useContext, useEffect, useState } from "react";
 
-const Item = ({ item, setChoices }) => {
-	const { item_name, price, quantity, item_img } = item;
-	const [border, setBorder] = useState("transparent");
-	const handleSelect = () => {
-		setBorder((current) => {
-			return current === "transparent" ? "white" : "transparent";
-		});
-		setChoices((current) => {
-			if (current.includes(item)) {
-				return current.filter((choice) => {
-					return choice !== item;
-				});
-			} else {
-				return [...current, item];
-			}
-		});
-	};
-	return (
-		<View>
-			<View style={[styles.container, { borderColor: border }]}>
-				<Text
-					style={[styles.items, { textAlign: "left", flex: 0.1 }]}
-					onPress={handleSelect}
-				>
-					{quantity}
-				</Text>
-				<View style={{ flex: 0.1, justifyContent: "center" }}>
-					<Image style={{ height: 30, width: 30 }} source={{ uri: item_img }} />
-				</View>
+const Item = ({ item, setChoices, setCost }) => {
+    const { item_name, price, quantity, item_img } = item;
+    const [border, setBorder] = useState("transparent");
+    const handleSelect = () => {
+        setBorder((current) => {
+            return current === "transparent" ? "white" : "transparent";
+        });
+        setChoices((current) => {
+            if (current.includes(item)) {
+                return current.filter((choice) => {
+                    return choice !== item;
+                });
+            } else {
+                return [...current, item];
+            }
+        });
+        setCost((current) => {
+            return current + (price * quantity);
+        });
+    };
+    return (
+        <View>
+            <View style={[styles.container, { borderColor: border }]}>
+                <Text
+                    style={[styles.items, { textAlign: "left", flex: 0.1 }]}
+                    onPress={handleSelect}
+                >
+                    {quantity}
+                </Text>
+                <View style={{ flex: 0.1, justifyContent: "center" }}>
+                    <Image
+                        style={{ height: 30, width: 30 }}
+                        source={{ uri: item_img }}
+                    />
+                </View>
 
-				<Text style={[styles.items, { flex: 0.5 }]} onPress={handleSelect}>
-					{item_name}
-				</Text>
-				<Text style={styles.items} onPress={handleSelect}>
-					{price}🪙
-				</Text>
-			</View>
-		</View>
-	);
+                <Text
+                    style={[styles.items, { flex: 0.5 }]}
+                    onPress={handleSelect}
+                >
+                    {item_name}
+                </Text>
+                <Text style={styles.items} onPress={handleSelect}>
+                    {price}🪙
+                </Text>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flexDirection: "row",
-		borderWidth: 3,
-		borderRadius: 10,
-		backgroundColor: "white",
-		marginVertical: 2,
-	},
-	items: {
-		flex: 0.3,
-		textAlign: "center",
-		fontSize: 20,
-	},
+    container: {
+        flexDirection: "row",
+        borderWidth: 3,
+        borderRadius: 10,
+        backgroundColor: "white",
+        marginVertical: 2,
+    },
+    items: {
+        flex: 0.3,
+        textAlign: "center",
+        fontSize: 20,
+    },
 });
 
 export default Item;
