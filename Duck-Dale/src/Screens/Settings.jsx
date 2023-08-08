413497ca0b17942493b606cd460103ca5ea283cd
LucyRTCodes <lucyrtutton@gmail.com>
2023-08-08 14:59:51 +0100

    fix merge conflict with settings

--- Duck-Dale/src/Screens/Settings.jsx
+++ Duck-Dale/src/Screens/Settings.jsx
@@ -1,9 +1,9 @@
 import {
-  ImageBackground,
-  Text,
-  View,
-  TouchableOpacity,
-  StyleSheet,
+	ImageBackground,
+	Text,
+	View,
+	TouchableOpacity,
+	StyleSheet,
 } from "react-native";
 import { useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -12,10 +12,9 @@ import { UserContext } from "../Contexts/UserContext";
 import * as ScreenOrientation from "expo-screen-orientation";
 
 function Settings() {
-  const { user, setUser } = useContext(UserContext);
-  const nav = useNavigation();
+	const { user, setUser } = useContext(UserContext);
+	const nav = useNavigation();
 
-<<<<<<< HEAD
 	const logout = () => {
 		// AsyncStorage.clear();
 		// this.props.navigate.nav;
@@ -30,114 +29,99 @@ function Settings() {
 	};
 
 	return (
-		// <View
-		//   style={{
-		//     flex: 1,
-		//     flexDirection: "row",
-		//     flexWrap: "wrap",
-		//     paddingHorizontal: 20,
-		//   }}
-		// >
-=======
-  const logout = () => {
-    setUser(null);
-  };
+		<View style={styles.container}>
+			<ImageBackground
+				source={require("../../assets/backgrounds/calm-anime-countryside.png")}
+				resizeMode="cover"
+				style={{
+					flex: 1,
+					justifyContent: "center",
+				}}
+			>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-evenly",
+						alignItems: "center",
+					}}
+				>
+					<View
+						style={[
+							styles.container,
+							{ flexDirection: "row", gap: 250, backgroundColor: "#ffffffdd" },
+						]}
+					>
+						<Text style={styles.text}>Welcome {user}</Text>
+					</View>
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../../assets/backgrounds/calm-anime-countryside.png")}
-        resizeMode="cover"
-        style={{
-          flex: 1,
-          justifyContent: "center",
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-          }}
-        >
-          <View
-            style={[
-              styles.container,
-              { flexDirection: "row", gap: 250, backgroundColor: "#ffffffdd" },
-            ]}
-          >
-            <Text style={styles.text}>Welcome {user}</Text>
-          </View>
->>>>>>> 81609eba8864fb86f98ce5822fbc98fb62341b14
-
-          <View>
-            <TouchableOpacity
-              onPress={() => nav.navigate("UpdateProfile")}
-              style={styles.pressable}
-            >
-              {/* <Image
+					<View>
+						<TouchableOpacity
+							onPress={() => nav.navigate("UpdateProfile")}
+							style={styles.pressable}
+						>
+							{/* <Image
                 source={require("../")} ///// could be a photo or text illustrating change in details
                 style={{ maxWidth: 150, maxHeight: 150 }}
               /> */}
-              <Text style={styles.text}>Update Profile</Text>
-            </TouchableOpacity>
+							<Text style={styles.text}>Update Profile</Text>
+						</TouchableOpacity>
 
-            <TouchableOpacity
-            //   onPress={Music} // music
-              style={styles.pressable}
-            >
-              {/* <Image
+						<TouchableOpacity
+							//   onPress={Music} // music
+							style={styles.pressable}
+						>
+							{/* <Image
                 source={require("../")} ///// could be an image to switch on or off music
                 style={{ maxWidth: 150, maxHeight: 150 }}
               /> */}
-              <Text style={styles.text}> Music </Text>
-            </TouchableOpacity>
+							<Text style={styles.text}> Music </Text>
+						</TouchableOpacity>
 
-            <TouchableOpacity
-            //   onPress={auto - save} ///
-              style={styles.pressable}
-            >
-              {/* <Image
+						<TouchableOpacity
+							//   onPress={auto - save} ///
+							style={styles.pressable}
+						>
+							{/* <Image
                 source={require("../")} ///// could be an image / button of an autosave
                 style={{ maxWidth: 150, maxHeight: 150 }}
               /> */}
-              <Text style={styles.text}>How To Play</Text>
-            </TouchableOpacity>
+							<Text style={styles.text}>How To Play</Text>
+						</TouchableOpacity>
 
-            <TouchableOpacity onPress={logout} style={styles.pressable}>
-              {/* <Image
+						<TouchableOpacity onPress={logout} style={styles.pressable}>
+							{/* <Image
                 source={require("../")} ///// could be an image / button to SIGN OUT
                 style={{ maxWidth: 150, maxHeight: 150 }}
               /> */}
-              <Text style={styles.text}>Sign Out</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </ImageBackground>
-    </View>
-  );
+							<Text style={styles.text}>Sign Out</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "centre",
-    justifyContent: "center",
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 20,
-    backgroundColor: "transparent",
-  },
-  img: {
-    borderWidth: 2,
-  },
-  pressable: {
-    borderColor: "white",
-    borderWidth: 3,
-    backgroundColor: "white",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "centre",
+		justifyContent: "center",
+	},
+	text: {
+		textAlign: "center",
+		fontSize: 20,
+		backgroundColor: "transparent",
+	},
+	img: {
+		borderWidth: 2,
+	},
+	pressable: {
+		borderColor: "white",
+		borderWidth: 3,
+		backgroundColor: "white",
+	},
 });
 
 export default Settings;
