df01a056e7e1f2e43bdd1ae555ff2888e4f1137d
LucyRTCodes <lucyrtutton@gmail.com>
2023-08-09 10:42:32 +0100

    merge duck in settings

--- Duck-Dale/src/Screens/Settings.jsx
+++ Duck-Dale/src/Screens/Settings.jsx
@@ -1,9 +1,9 @@
 import {
-  ImageBackground,
-  Text,
-  View,
-  TouchableOpacity,
-  StyleSheet,
+	ImageBackground,
+	Text,
+	View,
+	TouchableOpacity,
+	StyleSheet,
 } from "react-native";
 import { useContext } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -11,21 +11,21 @@ import { UserContext } from "../Contexts/UserContext";
 import * as ScreenOrientation from "expo-screen-orientation";
 
 function Settings() {
-  const { user, setUser } = useContext(UserContext);
-  const nav = useNavigation();
+	const { user, setUser } = useContext(UserContext);
+	const nav = useNavigation();
 
-  const logout = () => {
-    // AsyncStorage.clear();
-    // this.props.navigate.nav;
-    lockOrientation();
-    setUser(null);
-  };
+	const logout = () => {
+		// AsyncStorage.clear();
+		// this.props.navigate.nav;
+		lockOrientation();
+		setUser(null);
+	};
 
-  const lockOrientation = async () => {
-    await ScreenOrientation.lockAsync(
-      ScreenOrientation.OrientationLock.PORTRAIT
-    );
-  };
+	const lockOrientation = async () => {
+		await ScreenOrientation.lockAsync(
+			ScreenOrientation.OrientationLock.PORTRAIT
+		);
+	};
 
 	return (
 		<View style={styles.container}>
@@ -44,45 +44,67 @@ function Settings() {
 						alignItems: "center",
 					}}
 				>
-					<View
+					{/* <View
 						style={[
 							styles.container,
 							{ flexDirection: "row", gap: 250, backgroundColor: "#ffffffdd" },
 						]}
 					>
 						<Text style={styles.text}>Welcome {user}</Text>
+					</View> */}
+
+					<View>
+						<TouchableOpacity
+							onPress={() => nav.navigate("UpdateProfile")}
+							style={styles.pressable}
+						>
+							<Text style={styles.text}>Update Profile</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							//   onPress={Music} // music
+							style={styles.pressable}
+						>
+							<Text style={styles.text}> Music </Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							//   onPress={auto - save} ///
+							style={styles.pressable}
+						>
+							<Text style={styles.text}>How To Play</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity onPress={logout} style={styles.pressable}>
+							<Text style={styles.text}>Sign Out</Text>
+						</TouchableOpacity>
 					</View>
-
-          <View>
-            <TouchableOpacity
-              onPress={() => nav.navigate("UpdateProfile")}
-              style={styles.pressable}
-            >
-              <Text style={styles.text}>Update Profile</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-              //   onPress={Music} // music
-              style={styles.pressable}
-            >
-              <Text style={styles.text}> Music </Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-			//   onPress={auto - save} ///
-            style={styles.pressable}>
-              <Text style={styles.text}>How To Play</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={logout} style={styles.pressable}>
-              <Text style={styles.text}>Sign Out</Text>
-            </TouchableOpacity>
-
-          </View>
-        </View>
-      </ImageBackground>
-    </View>
-  );
+				</View>
+			</ImageBackground>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "centre",
+		justifyContent: "center",
+	},
+	text: {
+		textAlign: "center",
+		fontSize: 20,
+		backgroundColor: "transparent",
+	},
+	img: {
+		borderWidth: 2,
+	},
+	pressable: {
+		borderColor: "white",
+		borderWidth: 3,
+		backgroundColor: "white",
+	},
+});
 
 export default Settings;
