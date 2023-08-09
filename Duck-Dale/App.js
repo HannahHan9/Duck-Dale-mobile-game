eb36f83fbca100dbff967e3e8a652102dad05f28
DeanDevine <deanrussondevine@gmail.com>
2023-08-09 10:45:24 +0100

    set interval

--- Duck-Dale/App.js
+++ Duck-Dale/App.js
@@ -4,48 +4,26 @@ import { UserContext, UserProvider } from "./src/Contexts/UserContext";
 import { NavigationContainer } from "@react-navigation/native";
 import { CoinProvider } from "./src/Contexts/CoinContext";
 import { NewUserProvider } from "./src/Contexts/NewUserContext";
+import { SeasonContext, SeasonProvider } from "./src/Contexts/SeasonContext";
+import { useContext } from "react";
 
 LogBox.ignoreLogs(["new NativeEventEmitter"]);
 LogBox.ignoreAllLogs();
 
 export default function App() {
 
-
-//   React.useEffect(() => {
-//     return sound
-//       ? () => {
-//           console.log("Unloading Sound");
-//           sound.unloadAsync();
-//         }
-//       : undefined;
-//   }, [sound]); // SOUND
-
-// return (
-//     <View style={styles.container}>
-//       <Button title={status?'Pause Sound':'Play Sound'} onPress={onPressButton} />
-//     </View>
-//   );
-// }
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     backgroundColor: '#ECF0F1',
-//     padding: 10,
-//   },
-// });
-
-
     return (
-        <NewUserProvider>
-            <UserProvider>
-                <CoinProvider>
-                    <NavigationContainer>
-                        <AuthNavigator style={styles.container} />
-                    </NavigationContainer>
-                </CoinProvider>
-            </UserProvider>
-        </NewUserProvider>
+      <NewUserProvider>
+        <UserProvider>
+          <CoinProvider>
+            <SeasonProvider>
+              <NavigationContainer>
+                <AuthNavigator style={styles.container} />
+              </NavigationContainer>
+            </SeasonProvider>
+          </CoinProvider>
+        </UserProvider>
+      </NewUserProvider>
     );
 }
 
